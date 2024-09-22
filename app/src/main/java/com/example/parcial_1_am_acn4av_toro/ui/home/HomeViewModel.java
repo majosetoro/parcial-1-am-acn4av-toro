3ce7b25dc4ae46dbc0308fa9b57a24d7ae426954
majosetoro <toromajose@gmail.com>
2024-09-22 16:22:07 -0300

    Initial commit

---
+++ app/src/main/java/com/example/parcial_1_am_acn4av_toro/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.example.parcial_1_am_acn4av_toro.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
