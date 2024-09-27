3eca48f29342ff0d20a40589fe533152fa84c106
majosetoro <toromajose@gmail.com>
2024-09-27 00:06:05 -0300

    feat: add cancel button functionality

--- app/src/main/java/com/example/parcial_1_am_acn4av_toro/MainActivity.java
+++ app/src/main/java/com/example/parcial_1_am_acn4av_toro/MainActivity.java
@@ -52,20 +52,35 @@ public class MainActivity extends AppCompatActivity {
         edit_valor_Supermercado = findViewById(R.id.edit_valor_Supermercado);
         edit_valor_hogar = findViewById(R.id.edit_valor_hogar);
 
-        // Inicializamos el total.
+        // Inicializar el total.
         TextView tv_total;
         tv_total = findViewById(R.id.tv_total);
 
         Button button_Agregar = findViewById(R.id.button_Agregar);
+        Button button_cancelar = findViewById(R.id.button_cancelar);
         LinearLayout category_layout = findViewById(R.id.category_layout);
 
-        // Evento del botón 'Agregar'
+        // Evento del botón 'Agregar'.
         button_Agregar.setOnClickListener(v -> {
             double total = sumarGastos();
             tv_total.setText("Total: $" + total);
         });
+
+        // Evento del botón 'Cancelar'
+        button_cancelar.setOnClickListener(v -> {
+            // Se limpian todos los EditText
+            edit_valor_alquiler.setText("");
+            edit_valor_expensas.setText("");
+            edit_valor_servicios.setText("");
+            edit_valor_Supermercado.setText("");
+            edit_valor_hogar.setText("");
+
+            // Se reestablece el total a "$0.0"
+            tv_total.setText("Total: $0.0");
+        });
     }
 
+    // Se suman los gastos para obtener el valor.
     private double sumarGastos() {
         double alquiler = getValor((EditText) edit_valor_alquiler);
         double expensas = getValor((EditText) edit_valor_expensas);
@@ -76,6 +91,8 @@ public class MainActivity extends AppCompatActivity {
         return alquiler + expensas + servicios + supermercado + hogar;
     }
 
+
+
     private double getValor(EditText editText) {
         String texto = editText.getText().toString();
         if (texto.isEmpty()) {
@@ -83,5 +100,5 @@ public class MainActivity extends AppCompatActivity {
         }
         return Double.parseDouble(texto);
     }
-
 }
+
