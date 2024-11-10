6d5e361d65be2206e503f9bae28621b90cf9a078
majosetoro <toromajose@gmail.com>
2024-11-09 22:19:36 -0300

    update LoginActivity

--- app/src/main/java/com/example/parcial_2_am_acn4av_toro/LoginActivity.java
+++ app/src/main/java/com/example/parcial_2_am_acn4av_toro/LoginActivity.java
@@ -6,17 +6,26 @@ import android.view.View;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
     }
 
     public void onClick(View view){
@@ -28,6 +37,16 @@ public class LoginActivity extends AppCompatActivity {
 
         Log.i("firebase mandoca", email);
         Log.i("firebase mandoca", password);
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isComplete()) {
+                            Log.i("firebase-log", mAuth.getCurrentUser().getEmail());
+                        }
+                    }
+                });
     }
 
 }
