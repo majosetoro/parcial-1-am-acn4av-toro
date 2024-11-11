d20f09e114cbe3ce3021cadb1a30fa48a8b7c280
majosetoro <toromajose@gmail.com>
2024-11-10 21:05:24 -0300

    feat: update MainActivity.java

--- app/src/main/java/com/example/parcial_2_am_acn4av_toro/MainActivity.java
+++ app/src/main/java/com/example/parcial_2_am_acn4av_toro/MainActivity.java
@@ -40,8 +40,6 @@ public class MainActivity extends AppCompatActivity {
         if(currentUser != null) {
             mAuth.signOut(); // Cerrar sesión.
             setContentView(R.layout.activity_main);
-            //download();
-            currentUser.reload();
         } else {
             Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
@@ -51,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+        BottomNavigationView navView = findViewById(R.id.navView);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
