12b85af95adfb1c21630c86c00229b6083bc2468
majosetoro <toromajose@gmail.com>
2024-11-17 12:36:52 -0300

    Revert "update LoginActivity"
    This reverts commit 6d5e361d65be2206e503f9bae28621b90cf9a078.

--- app/src/main/java/com/example/parcial_2_am_acn4av_toro/LoginActivity.java
+++ app/src/main/java/com/example/parcial_2_am_acn4av_toro/LoginActivity.java
@@ -6,26 +6,17 @@ import android.view.View;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-
 public class LoginActivity extends AppCompatActivity {
-
-    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        mAuth = FirebaseAuth.getInstance();
     }
 
     public void onClick(View view){
@@ -37,16 +28,6 @@ public class LoginActivity extends AppCompatActivity {
 
         Log.i("firebase mandoca", email);
         Log.i("firebase mandoca", password);
-
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isComplete()) {
-                            Log.i("firebase-log", mAuth.getCurrentUser().getEmail());
-                        }
-                    }
-                });
     }
 
 }
