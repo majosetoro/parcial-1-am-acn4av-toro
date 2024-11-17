3941a261ec6e7151ff111e89b7672deff7af8bf6
majosetoro <toromajose@gmail.com>
2024-11-17 12:37:26 -0300

    Revert "upgrade build.gradle.kts"
    This reverts commit 6a5f6d7b3c92f657b2ed0fa7b1d699d9cf826053.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,6 @@ dependencies {
     implementation(libs.androidx.navigation.ui)
     implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
     implementation("com.google.firebase:firebase-auth:23.1.0")
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.navigation.fragment)
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
